954d91c92712669ad84b8ec69b6903a7a0a83851
Miles Tuffs <milest94@gmail.com>
2016-08-23 15:11:38 +0200

    Initial working copy of basic messaging server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', function(req, res){
+  res.render('index', {});
 });
 
-module.exports = router;
+module.exports = app;
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
